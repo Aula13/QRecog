fcf05e7710084bca3ae46177e58e1fb54cfaed00
danieleciriello <ciriello.daniele@gmail.com>
2014-05-13 11:07:36 +0200

    adds acquired datasets

---
+++ CameraTest/CMakeFiles/pcl_visualizer_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcl_visualizer_demo.dir/main.cpp.o"
+  "pcl_visualizer_demo.pdb"
+  "pcl_visualizer_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcl_visualizer_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
