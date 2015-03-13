33d5b0d2ea3ffbc7aeff6bb84f5c21f7ed9db568
st.pessina <st.pessina@gmail.com>
2015-03-13 15:01:29 +0100

    Save file captured features for acquisition

--- project/acquisitionview.cpp
+++ project/acquisitionview.cpp
@@ -7,10 +7,33 @@ AcquisitionView::AcquisitionView(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    ui->widget->setModelReference(Models::pcs);
+    ui->wgtPCLViewer->setModelReference(Models::pcs);
+    ui->wgtFIleChooser->asFileSaver();
+    Logger::logInfo("Acquisition view initialized");
 }
 
 AcquisitionView::~AcquisitionView()
 {
     delete ui;
+    Logger::logInfo("Acquisition view deleted");
+}
+
+void AcquisitionView::on_btnCapture_clicked()
+{
+    std::string filename = ui->wgtFIleChooser->getSelectedFile();
+    if(!filename.empty())
+    {
+        if(Models::pcs->getLastAcquisition()==NULL)
+        {
+            pcl::io::savePCDFileASCII(filename, *Models::pcs->getLastAcquisition());
+            Logger::logInfo("Acquisition PCD captured");
+            QMessageBox::information(this, "Information", "Filename saved!", QMessageBox::Ok);
+        } else {
+            QMessageBox::warning(this, "Error", "Nothing to acquire!", QMessageBox::Ok);
+            Logger::logWarning("Acquisition PCD try capture, nothing to acquire");
+        }
+    } else {
+        QMessageBox::warning(this, "Error", "Filename is empty!", QMessageBox::Ok);
+        Logger::logWarning("Acquisition PCD try capture, filename is empty");
+    }
 }
--- project/acquisitionview.h
+++ project/acquisitionview.h
@@ -2,8 +2,12 @@
 #define AcquisitionView_H
 
 #include <QWidget>
+#include <QMessageBox>
+
+#include <string>
 
 #include "models.h"
+#include <pcl/io/pcd_io.h>
 
 namespace Ui {
 class AcquisitionView;
@@ -16,6 +20,9 @@ class AcquisitionView : public QWidget
 public:
     explicit AcquisitionView(QWidget *parent = 0);
     ~AcquisitionView();
+
+private slots:
+    void on_btnCapture_clicked();
 
 private:
     Ui::AcquisitionView *ui;
